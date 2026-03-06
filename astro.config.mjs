28dcf7c648ded348e444c2ca6d4ee8c0390d7760
captmeds <medsdeb74@gmail.com>
2026-03-06 23:54:04 +0700

    Configure Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://balisunnykids.com',
+  output: 'static',
+  adapter: cloudflare(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
