11a15b087262112bf20d82ebdf5aa26de595edd0
captmeds <medsdeb74@gmail.com>
2026-03-07 03:50:03 +0700

    Switch to static output for standard Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://balisunnykids.com',
   output: 'static',
-  adapter: cloudflare(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
